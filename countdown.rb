19a6f9a41d12deff78bb316301ce043ee28343fc
Elliott Vargo <vargoelliott@gmail.com>
2021-01-17 00:19:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,9 +10,10 @@ end
 x = 10
 def countdown_with_sleep(x)
   while x > 1
+      puts "#{x} SECOND(S)!"
     x -= 1
     sleep 2
-    puts "#{x} SECOND(S)!"
+
   end
   return "HAPPY NEW YEAR!"
 end
